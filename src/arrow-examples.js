2b75394f147948a5dfc4e2f94acad0e301421740
NenadMilovanovic <milovanovicnenad207@gmail.com>
2025-12-29 19:21:41 +0100

    Learning how to use functions

---
+++ src/arrow-examples.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+
+const square = x => x * x;
+
+console.log('add(2,3)=', add(2, 3));
+console.log('multiply(2,3)=', multiply(2, 3));
+console.log('square(5)=', square(5));
